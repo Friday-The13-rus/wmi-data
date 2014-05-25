a8e9015df429867368baea2646745f6293ca8bd0
RogerUP.Rzn@gmail.com <RogerUP.Rzn@gmail.com@8f2a744d-f811-5a28-3a99-9ff0f3b1d9bd>
2014-05-25 09:25:36 +0000

    version 1.0.0.1 - refactoring code

---
+++ trunk/WMI/WMI/DataClasses/Drive.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WMI.DataClasses
+{
+	[ComVisible(true), ClassInterface(ClassInterfaceType.AutoDual)]
+	public class Drive
+	{
+		public string name;
+		public string volumeName;
+		public ulong freeSpace;
+		public byte usePercent;
+		public byte activePercent;
+
+		public Drive(string name, string volumeName, ulong freeSpace, byte usePercent, byte activePercent)
+		{
+			this.name = name;
+			this.volumeName = volumeName;
+			this.freeSpace = freeSpace;
+			this.usePercent = usePercent;
+			this.activePercent = activePercent;
+		}
+	}
+}
---
+++ trunk/WMI/WMI/DataReturner.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.InteropServices;
+using WMI.DataClasses;
+
+namespace WMI
+{
+	[ComVisible(true),
+	GuidAttribute("A79AC85C-547C-3ED3-AD94-530DC4BBB672"),
+	ProgId("WMI.GetData"),
+	ClassInterface(ClassInterfaceType.None)]
+	public class DataReturner : IWMI
+	{
+		DataManager data;
+
+		public void Start()
+		{
+			data = new DataManager(1000);
+		}
+
+		public void Stop()
+		{
+			data.Dispose();
+		}
+
+		public Drive GetDriveData(int i)
+		{
+			return data.GetDriveData(i);
+		}
+
+		public int GetDrivesCount()
+		{
+			return data.GetDrivesCount();
+		}
+
+		public Core GetProcessorData(int i)
+		{
+			return data.GetProcessorData(i);
+		}
+
+		public int GetCoresCount()
+		{
+			return data.GetCoresCount();
+		}
+
+		public NetworkInterface GetNetworkData()
+		{
+			return data.GetNetworkData();
+		}
+	}
+}
