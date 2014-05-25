57ddf4277037412e8f26baa1abe6ddcb50fed164
RogerUP.Rzn@gmail.com <RogerUP.Rzn@gmail.com@8f2a744d-f811-5a28-3a99-9ff0f3b1d9bd>
2014-05-25 07:25:18 +0000

    upload project

---
+++ WMI/WMI/Classes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace WMI
+{
+	[ComVisible(true), ClassInterface(ClassInterfaceType.AutoDual)]
+    public class Container
+    {
+        public List<Drive> drivers;
+		public List<Core> processor;
+		public NetworkInterface network;
+
+		public Container()
+		{
+			drivers = new List<Drive>();
+			processor = new List<Core>();
+			network = new NetworkInterface();
+		}
+    }
+
+    [ComVisible(true), ClassInterface(ClassInterfaceType.AutoDual)]
+    public class Drive
+    {
+        public string name;
+		public string volumeName;
+        public UInt64 freeSpace;
+		public UInt64 usePercent;
+		public UInt64 activePercent;
+    }
+
+	[ComVisible(true), ClassInterface(ClassInterfaceType.AutoDual)]
+    public class Core
+    {
+		public string name;
+		public UInt64 usePercent;
+    }
+
+	[ComVisible(true), ClassInterface(ClassInterfaceType.AutoDual)]
+	public class NetworkInterface
+	{
+		public UInt32 received;
+		public UInt32 sent;
+	}
+}
