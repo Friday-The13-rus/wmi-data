c09cd059f1e938fe38596276e705d0de8a97b524
RogerUP.Rzn@gmail.com <RogerUP.Rzn@gmail.com@8f2a744d-f811-5a28-3a99-9ff0f3b1d9bd>
2014-10-27 09:23:42 +0000

    possible resolve bug with crash on startup added test project for ManagementEventWatcher method receiving events

---
+++ trunk/WMI/Test/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string QueryString = "SELECT * FROM __InstanceOperationEvent WITHIN 1 WHERE " +
+				"TargetInstance isa 'Win32_PerfFormattedData_PerfOS_Processor' Or " +
+				"TargetInstance isa 'Win32_PerfFormattedData_PerfDisk_LogicalDisk' Or " +
+				"TargetInstance isa 'Win32_PerfFormattedData_Tcpip_NetworkInterface'";
+			using (ManagementEventWatcher watcher = new ManagementEventWatcher(QueryString))
+			{
+				watcher.EventArrived += watcher_EventArrived;
+				watcher.Stopped += watcher_Stopped;
+
+				Console.WriteLine("Watcher started");
+				watcher.Start();
+
+				Thread.Sleep(10000);
+				watcher.Stop();
+			}
+		}
+
+		static void watcher_Stopped(object sender, StoppedEventArgs e)
+		{
+			Console.WriteLine("Watcher stoped");
+		}
+
+		static void watcher_EventArrived(object sender, EventArrivedEventArgs e)
+		{
+			var obj = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+
+			string outLine;
+
+			switch ((string)obj["__Class"])
+			{
+				case "Win32_PerfFormattedData_PerfOS_Processor":
+					{
+						outLine = String.Format("Core: {0} Percent: {1} ({2})", obj["Name"], obj["PercentProcessorTime"], e.NewEvent["__CLASS"]);
+						break;
+					}
+				case "Win32_PerfFormattedData_PerfDisk_LogicalDisk":
+					{
+						outLine = String.Format("Disk: {0} Percent: {1} ({2})", obj["Name"], obj["PercentDiskTime"], e.NewEvent["__CLASS"]);
+						break;
+					}
+				case "Win32_PerfFormattedData_Tcpip_NetworkInterface":
+					{
+						outLine = String.Format("Received: {0} Sent: {1} ({2})", obj["BytesReceivedPerSec"], obj["BytesSentPerSec"], e.NewEvent["__CLASS"]);
+						break;
+					}
+				default:
+					{
+						outLine = String.Empty;
+						break;
+					}
+			}
+
+			Console.WriteLine(outLine);
+
+		}
+	}
+}
