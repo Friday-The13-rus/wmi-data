f9681aa3253bd24c528cb2754f5ca18fae3b1366
RogerUP.Rzn@gmail.com <RogerUP.Rzn@gmail.com@8f2a744d-f811-5a28-3a99-9ff0f3b1d9bd>
2014-10-28 07:01:16 +0000

    added read/write lock to cpu query

---
+++ WMI/WMI/ReaderWriterLockSlimExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace WMI
+{
+	public static class ReaderWriterLockSlimExtensions
+	{
+		public static void WriteLock(this ReaderWriterLockSlim locker, Action action)
+		{
+			locker.EnterWriteLock();
+			try
+			{
+				action();
+			}
+			finally
+			{
+				locker.ExitWriteLock();
+			}
+		}
+
+		public static T ReadLock<T>(this ReaderWriterLockSlim locker, Func<T> func)
+		{
+			locker.EnterReadLock();
+			try
+			{
+				return func();
+			}
+			finally
+			{
+				locker.ExitReadLock();
+			}
+		}
+	}
+}
