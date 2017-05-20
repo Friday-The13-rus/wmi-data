ac5849a26f8c4c88bd6d7491771737393ebd8287
Antony <RogerUP13@users.noreply.github.com>
2017-05-20 15:01:31 +0300

    Updated debugging application

--- WMI/TestDll/Program.cs
+++ WMI/TestDll/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 using WMI;
 using Timer = System.Timers.Timer;
 
@@ -6,38 +7,20 @@ namespace TestDll
 {
 	class Program
 	{
-		readonly static Timer Timer = new Timer(1000);
-
 		static void Main(string[] args)
 		{
-			DataReturner dr = new DataReturner();
-			dr.Start();
-
-			WriteData(dr);
-
-			Console.ReadLine();
-
-			Timer.Stop();
-
-			dr.Stop();
+			using (var timer = new Timer(1000))
+			using (var dataManager = new DataManager(1000))
+			{
+				timer.Elapsed += (sender, eventArgs) => { DumpData(dataManager); };
+				timer.Start();
+				Console.ReadLine();
+			}
 		}
 
-		private static void WriteData(DataReturner dr)
+		private static void DumpData(DataManager dr)
 		{
-			Timer.Elapsed += (sender, args) =>
-			{
-				//for (int i = 0; i < dr.GetCoresCount(); i++)
-				//{
-				//	var core = dr.GetProcessorData(i);
-				//	Console.WriteLine("C {0} - {1}", core.Name, core.UsePercent);
-				//}
-				//for (int i = 0; i < dr.GetDrivesCount(); i++)
-				//{
-				//	var drive = dr.GetDriveData(i);
-				//	Console.WriteLine("D {0} - {1}", drive.Name, drive.ActivePercent);
-				//}
-			};
-			Timer.Start();
+			Console.WriteLine(JsonConvert.SerializeObject(dr.GetDriveData(), Formatting.Indented));
 		}
 	}
 }
